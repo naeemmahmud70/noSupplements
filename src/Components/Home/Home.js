013e5324b3f26b495b7da355f997410baa54283b
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-12-17 19:24:43 +0600

    i have forgotted to implement the preloader features that wwhy now i am adding this features

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Preloader from '../Preloader/Preloader';
 import Users from '../Users/Users';
 
 const Home = () => {
@@ -9,9 +10,16 @@ const Home = () => {
             .then(res => res.json())
             .then(data => setUsers(data))
     }, []);
-    
+
     return (
         <section className="row d-flex justify-content-center">
+            <div>
+                {
+                    users.length === 0 && <div>
+                        <Preloader></Preloader>
+                    </div>
+                }
+            </div>
             {
                 users.map(user => <Users user={user} key={user.id}></Users>)
             }
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Preloader from '../Preloader/Preloader';
 
 
 const Users = ({ user }) => {
